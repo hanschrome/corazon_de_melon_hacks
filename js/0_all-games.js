f0ae2c506faab43d5405c94167b774e0aedbc7b8
Hans <hanscastroadmn@gmail.com>
2023-07-31 02:53:12 +0200

    Update 0_all-games.js

--- js/0_all-games.js
+++ js/0_all-games.js
@@ -108,81 +108,81 @@ $.ajax({
 
 // 'flower'
 
-var urlFlower = "https://api3.corazondemelon.es/v2/minigame/flower";
-var timestampFlower = new Date().getTime().toString();
-var signatureFlower = generateSignature(publicKey, privateKey, "GET", urlFlower, null, timestampFlower);
+// var urlFlower = "https://api3.corazondemelon.es/v2/minigame/flower";
+// var timestampFlower = new Date().getTime().toString();
+// var signatureFlower = generateSignature(publicKey, privateKey, "GET", urlFlower, null, timestampFlower);
 
-$.ajax({
-  url: urlFlower,
-  method: "GET",
-  headers: {
-    'X-Beemoov-Application': publicKey,
-    'X-Beemoov-Signature': signatureFlower,
-    'X-Beemoov-Timestamp': timestampFlower,
-  },
-  success: function(data) {
-    console.log(data);
+// $.ajax({
+//   url: urlFlower,
+//   method: "GET",
+//   headers: {
+//     'X-Beemoov-Application': publicKey,
+//     'X-Beemoov-Signature': signatureFlower,
+//     'X-Beemoov-Timestamp': timestampFlower,
+//   },
+//   success: function(data) {
+//     console.log(data);
 
-    // Segunda petición al juego de las flores (PUT)
-    var flowerId = data.data.id;
-    var urlFlower2nd = `https://api3.corazondemelon.es/v2/minigame/flower/${flowerId}`;
-    var bodyFlower2nd = JSON.stringify({});
-    var timestampFlower2nd = new Date().getTime().toString();
-    var signatureFlower2nd = generateSignature(publicKey, privateKey, "PUT", urlFlower2nd, bodyFlower2nd, timestampFlower2nd);
+//     // Segunda petición al juego de las flores (PUT)
+//     var flowerId = data.data.id;
+//     var urlFlower2nd = `https://api3.corazondemelon.es/v2/minigame/flower/${flowerId}`;
+//     var bodyFlower2nd = JSON.stringify({});
+//     var timestampFlower2nd = new Date().getTime().toString();
+//     var signatureFlower2nd = generateSignature(publicKey, privateKey, "PUT", urlFlower2nd, bodyFlower2nd, timestampFlower2nd);
 
-    $.ajax({
-      url: urlFlower2nd,
-      method: "PUT",
-      headers: {
-        'Content-Type': 'application/json',
-        'X-Beemoov-Application': publicKey,
-        'X-Beemoov-Signature': signatureFlower2nd,
-        'X-Beemoov-Timestamp': timestampFlower2nd,
-      },
-      data: bodyFlower2nd,
-      success: function(data) {
-        console.log(data);
-      }
-    });
-  }
-});
+//     $.ajax({
+//       url: urlFlower2nd,
+//       method: "PUT",
+//       headers: {
+//         'Content-Type': 'application/json',
+//         'X-Beemoov-Application': publicKey,
+//         'X-Beemoov-Signature': signatureFlower2nd,
+//         'X-Beemoov-Timestamp': timestampFlower2nd,
+//       },
+//       data: bodyFlower2nd,
+//       success: function(data) {
+//         console.log(data);
+//       }
+//     });
+//   }
+// });
 
-var urlCash = "https://api3.corazondemelon.es/v2/minigame/cash";
-var timestampCash = new Date().getTime().toString();
-var signatureCash = generateSignature(publicKey, privateKey, "GET", urlCash, null, timestampCash);
+// var urlCash = "https://api3.corazondemelon.es/v2/minigame/cash";
+// var timestampCash = new Date().getTime().toString();
+// var signatureCash = generateSignature(publicKey, privateKey, "GET", urlCash, null, timestampCash);
 
-$.ajax({
-  url: urlCash,
-  method: "GET",
-  headers: {
-    'X-Beemoov-Application': publicKey,
-    'X-Beemoov-Signature': signatureCash,
-    'X-Beemoov-Timestamp': timestampCash,
-  },
-  success: function(data) {
-    console.log(data);
+// $.ajax({
+//   url: urlCash,
+//   method: "GET",
+//   headers: {
+//     'X-Beemoov-Application': publicKey,
+//     'X-Beemoov-Signature': signatureCash,
+//     'X-Beemoov-Timestamp': timestampCash,
+//   },
+//   success: function(data) {
+//     console.log(data);
 
-    // Segunda petición al juego "rasca y gana" (PUT)
-    var cashId = data.data.id;
-    var urlCash2nd = `https://api3.corazondemelon.es/v2/minigame/cash/${cashId}`;
-    var bodyCash2nd = JSON.stringify({});
-    var timestampCash2nd = new Date().getTime().toString();
-    var signatureCash2nd = generateSignature(publicKey, privateKey, "PUT", urlCash2nd, bodyCash2nd, timestampCash2nd);
+//     // Segunda petición al juego "rasca y gana" (PUT)
+//     var cashId = data.data.id;
+//     var urlCash2nd = `https://api3.corazondemelon.es/v2/minigame/cash/${cashId}`;
+//     var bodyCash2nd = JSON.stringify({});
+//     var timestampCash2nd = new Date().getTime().toString();
+//     var signatureCash2nd = generateSignature(publicKey, privateKey, "PUT", urlCash2nd, bodyCash2nd, timestampCash2nd);
 
-    $.ajax({
-      url: urlCash2nd,
-      method: "PUT",
-      headers: {
-        'Content-Type': 'application/json',
-        'X-Beemoov-Application': publicKey,
-        'X-Beemoov-Signature': signatureCash2nd,
-        'X-Beemoov-Timestamp': timestampCash2nd,
-      },
-      data: bodyCash2nd,
-      success: function(data) {
-        console.log(data);
-      }
-    });
-  }
-});
+//     $.ajax({
+//       url: urlCash2nd,
+//       method: "PUT",
+//       headers: {
+//         'Content-Type': 'application/json',
+//         'X-Beemoov-Application': publicKey,
+//         'X-Beemoov-Signature': signatureCash2nd,
+//         'X-Beemoov-Timestamp': timestampCash2nd,
+//       },
+//       data: bodyCash2nd,
+//       success: function(data) {
+//         console.log(data);
+//       }
+//     });
+//   }
+// });
 
